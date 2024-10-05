f56f362866f54cb24479bbfe7b8387bbff26b9d5
DraconovRyan04 <draconov.teo.75@gmail.com>
2024-10-05 10:44:40 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import Greet from "./components/Greet.vue";
+import TwoMatrix from "./components/TwoMatrix.vue"
+</script>
+
+<template>
+  <div class="h-full w-full flex flex-row">
+    <div class="bg-gray-700 w-1/5 h-full p-3">
+      <div class="m-auto shadow-2xl text-center p-3">
+        Простые <br>операции
+      </div>
+      <div class="m-auto shadow-2xl text-center mt-4 p-3">
+        Линейные <br>уравнения
+      </div>
+    </div>
+    <div class="w-4/5 h-full p-3">
+      <TwoMatrix />
+    </div>
+  </div>
+</template>
---
+++ src/components/TwoMatrix.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-row h-3/5">
+    <div class="w-2/5 h-full">
+        <div class="w-full bg-slate-700 h-2/3">
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+    <div class="flex flex-col w-1/5 h-2/3 px-4 justify-end">
+        <div class="mx-auto bg-slate-700 text-center p-2 w-3/5">A+B</div>
+        <div class="mx-auto bg-slate-700 text-center p-2 w-3/5 mt-4">A-B</div>
+        <div class="mx-auto bg-slate-700 text-center p-2 w-3/5 mt-4">A*B</div>
+    </div>
+    <div class="w-2/5 h-full">
+        <div class="w-full bg-slate-700 h-2/3">
+
+        </div>
+        <div>
+
+        </div>
+    </div>
+  </div>
+  <div>
+
+  </div>
+</template>
